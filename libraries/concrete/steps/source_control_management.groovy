5e03a3dffda56677b78bea2406fcfd1863ef91c9
polaksh <polaksh@gmail.com>
2021-02-24 18:51:57 +0200

    Update source_control_management.groovy

--- libraries/concrete/steps/source_control_management.groovy
+++ libraries/concrete/steps/source_control_management.groovy
@@ -1,3 +1,11 @@
 void call(){
-    echo "This is my clone step"
+    stage('SCM'){
+        try{
+            echo "This is my clone step"
+        }
+        catch(Throwable t){
+            currentBuild.result = "FAILURE"     
+            currentBuild.description = groovy.json.JsonOutput.toJson([message: t.message])
+        }
+    }
 }
