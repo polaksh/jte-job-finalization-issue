5bbd260fd3f9b2e4013bae2abaeacf6de55aa625
Shlomi Polak <shlomi@transmitsecurity.com>
2021-07-05 19:41:43 +0300

    put json in pipelineCongif

--- libraries/concrete/steps/execute_build.groovy
+++ libraries/concrete/steps/execute_build.groovy
@@ -1,7 +1,7 @@
 void call(){
     //just to make this fail and reproduce my issue, this build will fail on an even BUILD_NUMBER
     if(env.BUILD_NUMBER.toInteger() % 2 == 0){
-        currentBuild.description = groovy.json.JsonOutput.toJson([message: '!!! EVEN NUMBERS ARE THE DEVIL !!!'])
+        pipelineConfig.description = groovy.json.JsonOutput.toJson([message: '!!! EVEN NUMBERS ARE THE DEVIL !!!'])
         error '!!! EVEN NUMBERS ARE THE DEVIL !!!'
     }
 }
