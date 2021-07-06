7f717cc6526b05b092b930aa7adc6353367c8c20
Shlomi Polak <shlomi@transmitsecurity.com>
2021-07-06 09:58:00 +0300

    try maps

--- libraries/concrete/steps/execute_build.groovy
+++ libraries/concrete/steps/execute_build.groovy
@@ -1,7 +1,9 @@
 void call(){
     //just to make this fail and reproduce my issue, this build will fail on an even BUILD_NUMBER
-    if(env.BUILD_NUMBER.toInteger() % 2 == 0){
-        pipelineConfig.description = groovy.json.JsonOutput.toJson([message: '!!! EVEN NUMBERS ARE THE DEVIL !!!'])
-        error '!!! EVEN NUMBERS ARE THE DEVIL !!!'
-    }
+    // if(env.BUILD_NUMBER.toInteger() % 2 == 0){
+    // pipelineConfig.description = groovy.json.JsonOutput.toJson([message: '!!! EVEN NUMBERS ARE THE DEVIL !!!'])
+    //     error '!!! EVEN NUMBERS ARE THE DEVIL !!!'
+    // }
+
+    pipelineConfig.description = [message: '!!! EVEN NUMBERS ARE THE DEVIL !!!']
 }
